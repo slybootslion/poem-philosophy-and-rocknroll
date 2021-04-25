d5f7be575c5a277b767724e10682c6695ba56f82
slybootslion <slybootslion@vip.qq.com>
2021-04-25 20:06:25 +0800

    2021-04-25 20:06:24

--- src/config/setting.js
+++ src/config/setting.js
@@ -1,7 +1,7 @@
 const NODE_ENV = process.env.NODE_ENV
 const isProduction = NODE_ENV && NODE_ENV.trim() === 'production'
 
-const baseURL = isProduction ? 'http://cyberpunk.slybootslion.com/' : 'http://localhost:39100'
+const baseURL = isProduction ? 'https://cyberpunk.slybootslion.com/' : 'http://localhost:39100'
 
 const baseOssURL = 'https://slybootslion.oss-cn-chengdu.aliyuncs.com/'
 
