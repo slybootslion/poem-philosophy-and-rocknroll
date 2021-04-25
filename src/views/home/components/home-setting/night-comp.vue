7362f91d33dbe0b48c1c0bb1f626dc8590c0e843
SlybootsLion <slybootslion@vip.qq.com>
2021-04-25 22:32:36 +0800

    2021-04-25 22:32:36

--- src/views/home/components/home-setting/night-comp.vue
+++ src/views/home/components/home-setting/night-comp.vue
@@ -2,17 +2,18 @@
   <home-setting-container title="夜间模式" desc="夜间模式开启选项">
     <home-setting-item text="是否开启"
                        desc="手动控制夜间模式">
-      <el-switch :value="store.getters.isNightModel"
+      <el-switch :value="getters.isNightModel"
                  @change="nightModelChange" />
     </home-setting-item>
     <home-setting-item text="自动设置"
                        desc="会根据时间自动设置，6am~10pm为白天模式，其他时间为夜间模式">
-      <el-switch :value="store.getters.isAutoNightModel"
+      <el-switch :value="getters.isAutoNightModel"
                  @change="autoNightModelChange" />
     </home-setting-item>
   </home-setting-container>
   <home-setting-container title="全屏" desc="是否开启全屏">
-    <home-setting-item text="是否全屏显示">
+    <home-setting-item text="是否全屏显示"
+                       hide>
       <el-switch v-model="isFullScreen" @change="fullScreen" />
     </home-setting-item>
   </home-setting-container>
@@ -32,14 +33,14 @@ export default {
   },
   setup () {
     const store = useStore()
-    const autoNightModelChange = async value => {
-      await store.dispatch('sys/setAutoNightAction', value)
-      await store.dispatch('sys/setNightAction', false)
+    const autoNightModelChange = async status => {
+      await store.dispatch('sys/setAutoNightAction', status)
+      await store.dispatch('sys/setNightAction', store.getters.isNight)
     }
 
     const nightModelChange = async value => {
-      await store.dispatch('sys/setAutoNightAction', false)
       await store.dispatch('sys/setNightAction', value)
+      await store.dispatch('sys/setAutoNightAction', false)
       await store.dispatch('sys/nightAction', value)
     }
 
@@ -57,7 +58,7 @@ export default {
     }
 
     return {
-      store,
+      getters: store.getters,
       autoNightModelChange,
       nightModelChange,
       fullScreen,
